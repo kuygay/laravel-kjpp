96fff6ceca9e8bd662174d4f6fd1976c6bcf5ecb
Denny F.A <kuygay@gmail.com>
2021-11-03 15:37:52 +0700

    Perbaikan tabel view PstKJPP

--- resources/views/page-kjpp.blade.php
+++ resources/views/page-kjpp.blade.php
@@ -1,6 +1,20 @@
-@foreach($items as $row)
-    {{ $row->cab_dr_kjpp }} <br>
-    {{ $row->no_izin }} <br>
-    {{ $row->tgl_izin }} <br>
-    <br>
-@endforeach
+<table width="100%" cellspacing="0" cellpadding="0" border="1">
+    <thead>
+        <th>No</th>
+        <th>Id</th>
+        <th>KJPP</th>
+        <th>Nomor Izin </th>
+        <th>tgl_izin</th>
+    </thead>
+    <tbody>
+        @foreach($items as $row)
+        <tr>
+            <td> {{ $i++ }}</td>
+            <td> {{ $row->id }} </td> 
+            <td> {{ $row->nama_kjpp }} </td> 
+            <td> {{ $row->no_induk }} </td>
+            <td> {{ $row->no_izin_kjpp }}</td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
